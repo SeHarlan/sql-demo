f8d55ba67ba87dfae3fcae08bcc375cf32b74aca
Scott Harlan <seharlan@gmail.com>
2020-02-20 17:16:10 -0800

    data seeded

---
+++ data/beers.js
@@ -0,0 +1,50 @@
+module.exports = [
+    {
+        name: 'Belgian White',
+        type: 'Wheat',
+        image: 'blueMoon.jpg',
+        brewery: 'Blue Moon',
+        alchoholic: true,
+        ABV: 5.4
+    },
+    {
+        name: 'Volatile Substance',
+        type: 'IPA',
+        image: 'volatileSubstance.jpg',
+        brewery: 'Von Ebert Brewing',
+        alchoholic: true,
+        ABV: 6.7
+    },
+    {
+        name: 'Quiescence',
+        type: 'Sour',
+        image: 'quiescence.jpg',
+        brewery: 'Breakside Brewery',
+        alchoholic: true,
+        ABV: 5.9
+    },
+    {
+        name: 'Jammy Pants',
+        type: 'Sour',
+        image: 'jammyPants.jpg',
+        brewery: 'Great Notion Brewing',
+        alchoholic: true,
+        ABV: 7.0
+    },
+    {
+        name: 'Yasuni',
+        type: 'IPA',
+        image: 'yasuni.jpg',
+        brewery: 'Old Town Brewing',
+        alchoholic: true,
+        ABV: 6.5
+    },
+    {
+        name: 'Heineken 0.0',
+        type: 'Lager',
+        image: 'heineken00.jpg',
+        brewery: 'Heineken',
+        alchoholic: false,
+        ABV: 0.0
+    },
+];
